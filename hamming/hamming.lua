2c54e446d27430658ddff853d76ff624560b8984
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-11-11 23:57:11 +0100

    Solve exercise "Hamming"

--- hamming/hamming.lua
+++ hamming/hamming.lua
@@ -1,7 +1,22 @@
 local Hamming = {}
 
-function Hamming.compute(a,b)
-
+Hamming.compute = function(a, b)
+  assert(type(a) == 'string', "strand `a` must be a string")
+  assert(type(b) == 'string', "strand `b` must be a string")
+  local hamming_d = 0
+  if string.len(a) ~= string.len(b) then
+    --error("the strands have not the same length")
+    hamming_d = -1
+  else
+    for i = 1, #a do
+      local c_a = a:sub(i, i)
+      local c_b = b:sub(i, i)
+      if c_a ~= c_b then
+        hamming_d = hamming_d + 1
+      end
+    end
+  end
+  return hamming_d
 end
 
 return Hamming
