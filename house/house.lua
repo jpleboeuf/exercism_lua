ff74b588f4a0eef04e85b90c4f2614af3b6af3a7
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-03-09 23:46:32 +0100

    Solve exercise "House" (cont.)
    Wrote a less dumb solution.

--- house/house.lua
+++ house/house.lua
@@ -1,85 +1,46 @@
 local house = {}
 
-local verses = {
-  [[This is the house that Jack built.]],
-  [[This is the malt
-that lay in the house that Jack built.]],
-  [[This is the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the farmer sowing his corn
-that kept the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]],
-  [[This is the horse and the hound and the horn
-that belonged to the farmer sowing his corn
-that kept the rooster that crowed in the morn
-that woke the priest all shaven and shorn
-that married the man all tattered and torn
-that kissed the maiden all forlorn
-that milked the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.]]
+local verses = {}
+
+-- "This is  the house  that Jack built."
+--  \_____/  \_/ \___/  \_____________/
+--            \______________________/
+--                  Noun Phrase:
+--  + "the": Determiner > Central Determiner > Article > Definite
+--  + "house": Head > Noun
+--  + "that Jack built": Postmodifier > Relative Clause
+
+local verses_elems = {
+  {['head'] = "house",   ['postmodifier'] = "that Jack built",                                      },
+  {['head'] = "malt",                                                  ['rel_verb'] = "lay in"      },
+  {['head'] = "rat",                                                   ['rel_verb'] = "ate"         },
+  {['head'] = "cat",                                                   ['rel_verb'] = "killed"      },
+  {['head'] = "dog",                                                   ['rel_verb'] = "worried"     },
+  {['head'] = "cow",     ['postmodifier'] = "with the crumpled horn",  ['rel_verb'] = "tossed"      },
+  {['head'] = "maiden",  ['postmodifier'] = "all forlorn",             ['rel_verb'] = "milked"      },
+  {['head'] = "man",     ['postmodifier'] = "all tattered and torn",   ['rel_verb'] = "kissed"      },
+  {['head'] = "priest",  ['postmodifier'] = "all shaven and shorn",    ['rel_verb'] = "married"     },
+  {['head'] = "rooster", ['postmodifier'] = "that crowed in the morn", ['rel_verb'] = "woke"        },
+  {['head'] = "farmer",  ['postmodifier'] = "sowing his corn",         ['rel_verb'] = "kept"        },
+  {['head'] = "horse and the hound and the horn",                      ['rel_verb'] = "belonged to" },
 }
+
+gen_verses = function()
+  for i = 1, #verses_elems do
+    verses[i] = "This is "
+    verses[i] = verses[i] .. "the " .. verses_elems[i]['head']
+    if verses_elems[i]['postmodifier'] then
+      verses[i] = verses[i] .. " " .. verses_elems[i]['postmodifier']
+    end
+    if verses_elems[i]['rel_verb'] then
+      verses[i] = verses[i] .. "\nthat " .. verses_elems[i]['rel_verb'] .. " "
+      verses[i] = verses[i] .. verses[i-1]:sub(9, verses[i-1]:len()-1)
+    end
+    verses[i] = verses[i] .. "."
+  end
+end
+
+gen_verses()
 
 house.verse = function(which)
   assert(type(which) == 'number' and (which >= 1 and which <= #verses),
